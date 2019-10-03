ef7454c78529da4bc66be03a9d92b3bd938f13b5
Sergey Rudenko <hzzh@outlook.com>
2019-10-03 18:40:23 +0300

    notifycations win 10 class

---
+++ UWP/Notyficator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Notifications;
+
+namespace Notyes
+{
+    class Notyficator
+    {
+        public static void Notify(string title = "", string message = "", string image_path = "")
+        {
+            // Get a toast XML template
+            var toastXml = ToastNotificationManager.GetTemplateContent(ToastTemplateType.ToastImageAndText04);
+
+            // Fill in the text elements
+            var stringElements = toastXml.GetElementsByTagName("text");
+            stringElements[0].AppendChild(toastXml.CreateTextNode(title));
+            stringElements[1].AppendChild(toastXml.CreateTextNode(message));
+
+            // Specify the absolute path to an image
+            if (image_path != "")
+            {
+                string imagePath = "file:///" + Path.GetFullPath(image_path);
+                var imageElements = toastXml.GetElementsByTagName("image");
+                imageElements[0].Attributes.GetNamedItem("src").NodeValue = imagePath;
+            }
+            // Create the toast and attach event listeners
+            ToastNotification toast = new ToastNotification(toastXml);
+
+            // Show the toast. Be sure to specify the AppUserModelId on your application's shortcut!
+            ToastNotificationManager.CreateToastNotifier().Show(toast);
+        }
+    }
+}
